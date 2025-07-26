bc5a68675e0fd524c61740454508f6be3a4b602f
XilingZHU <zxl041024@163.com>
2025-07-26 18:01:30 +0800

    静态资源创建

--- src/main.js
+++ src/main.js
@@ -1,16 +1,14 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
 
+import '@/styles/common.scss'
 // 测试接口函数
 import { getCategory } from './apis/testAPI'
 getCategory().then(res => {
   console.log(res);
-
 })
 const app = createApp(App)
 
