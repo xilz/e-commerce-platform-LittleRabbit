e32be7085ced2539d4f9703a8b26bdc6cb0bb503
XilingZHU <zxl041024@163.com>
2025-07-27 18:44:57 +0800

    一级分类-轮播图

--- src/views/Category/index.vue
+++ src/views/Category/index.vue
@@ -2,6 +2,7 @@
 import { getTopCategoryAPI } from '@/apis/category'
 import { onMounted, ref } from 'vue'
 import { useRoute } from 'vue-router'
+import { getBannerAPI } from '@/apis/home'
 
 const categoryData = ref({})
 const route = useRoute()
@@ -9,8 +10,18 @@ const getCategory = async () => {
   const res = await getTopCategoryAPI(route.params.id)
   categoryData.value = res.result
 }
-
 onMounted(() => getCategory())  // watchEffect
+
+const bannerList = ref([])
+const getBanner = async() => {
+  const res = await getBannerAPI({
+    distributionSite: '2'
+  })
+  console.log(res);
+  bannerList.value = res.result
+}
+
+onMounted(() => getBanner())
 </script>
 
 <template>
@@ -23,12 +34,31 @@ onMounted(() => getCategory())  // watchEffect
           <el-breadcrumb-item>{{ categoryData.name }}</el-breadcrumb-item>
         </el-breadcrumb>
       </div>
+      <!-- 轮播图 -->
+      <div class="home-banner">
+        <el-carousel height="500px">
+          <el-carousel-item v-for="item in bannerList" :key="item.id">
+            <img :src="item.imgUrl" alt="">
+          </el-carousel-item>
+        </el-carousel>
+      </div>
     </div>
   </div>
 </template>
 
 
 <style scoped lang="scss">
+.home-banner {
+  width: 1240px;
+  height: 500px;
+  margin: 0 auto;
+
+  img {
+    width: 100%;
+    height: 500px;
+  }
+}
+
 .top-category {
   h3 {
     font-size: 28px;
