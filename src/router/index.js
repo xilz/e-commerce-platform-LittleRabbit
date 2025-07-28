9e7dc6a5ce1fc5ff66ae3d81a577c4394b2060bb
XilingZHU <zxl041024@163.com>
2025-07-28 11:13:02 +0800

    二级分类-路由配置

--- src/router/index.js
+++ src/router/index.js
@@ -4,7 +4,8 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Login from '@/views/Login/index.vue'
 import Layout from '@/views/Layout/index.vue'
 import Home from '@/views/Home/index.vue'
-import Catecory from '@/views/Category/index.vue'
+import Category from '@/views/Category/index.vue'
+import SubCategory from '@/views/SubCategory/index.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -20,7 +21,11 @@ const router = createRouter({
         },
         {
           path:'category/:id',
-          component: Catecory
+          component: Category
+        },
+        {
+          path: 'category/sub/:id',
+          component: SubCategory
         }
       ]
     },
